656edc1c37d6c03d59deb1d5aec2215cdfe04cc1
Dish26 <dishaa119@gmail.com>
2021-08-01 00:16:12 +0530

    store.js changes

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -4,6 +4,7 @@ import {composeWithDevTools} from 'redux-devtools-extension';
 
 import {cartReducer} from './reducers/cartReducers';
 import { getProductsReducer,  getProductDetailsReducer} from './reducers/productReducers';
+// import { json } from 'express';
  
 const reducer = combineReducers({
     //passing an obj, and in here we'll have all our diffn reducers
@@ -15,8 +16,17 @@ const reducer = combineReducers({
 //array of all middlewares, but we have only 1 middleware here(thunk)
 const middleware = [thunk];
 
+const cartFromLocalstorage = localStorage.getItem("cart") ? JSON.parse(localStorage.getItem("cart")) : []
+
+const INITIAL_STATE = {
+    cart: {
+        cartItems: cartFromLocalstorage
+    }
+}
+
 const store = createStore(
     reducer,
+    INITIAL_STATE,
     composeWithDevTools(applyMiddleware(...middleware))
 );
 
